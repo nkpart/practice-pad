bf598e2b5b7899ea4b06ef22030de2c8358a7b2e
Nick Partridge <nkpart@gmail.com>
2017-07-07 17:03:23 +1000

    new style records

--- app.hs
+++ app.hs
@@ -35,152 +35,135 @@ newtype Seconds =
   Seconds Integer
   deriving (Eq, Show, Ord, Enum, Num)
 
-data LimitIns t =  LimitIns {
+data LimitInputs t =  LimitInputs {
     _limitInsIncrease :: Event t Seconds
   , _limitInsDecrease :: Event t Seconds
 }
 
-data LimitOuts t =  LimitOuts {
+data LimitOutputs t =  LimitOutputs {
     _limitOutsSeconds :: Dynamic t Seconds
 }
 
-createLimit :: MonadWidget t m => LimitIns t -> Seconds -> m (LimitOuts t)
+createLimit :: MonadWidget t m => LimitInputs t -> Seconds -> m (LimitOutputs t)
 createLimit input initial =
-  fmap LimitOuts .
+  fmap LimitOutputs .
   accum (&) initial . mergeWith (.) $ [
-    (-) <$> _limitInsDecrease input,
+    (\x y -> y - x) <$> _limitInsDecrease input,
     (+) <$> _limitInsIncrease input
   ]
 
 data TockerInputs t = TockerInputs {
-   _tockerInputsLimit :: Behavior t Int
-  ,_tockerInputsStart :: Event t ()
-  ,_tockerInputsReset :: Event t ()
-  ,_tockerInputsPause :: Event t ()
+   _tockerInputsLimit  :: Behavior t Seconds
+  ,_tockerInputsStart  :: Event t ()
+  ,_tockerInputsReset  :: Event t ()
+  ,_tockerInputsPause  :: Event t ()
+  ,_tockerInputsResume :: Event t ()
   }
 
 data TockerOutputs t = TockerOutputs {
-    timerElapsed  :: Event t Seconds
-  , timerAlarming :: Event t Bool
+    _tockerOutputsElapsed  :: Dynamic t Seconds
+  , _tockerOutputsAlarming :: Event t Bool
   }
 
-data Timer = Timer
-  { _timerBegin   :: Seconds
-  -- Nothing when the timer isn't running
-  , _timerCurrent :: Maybe Seconds
-  , _timerPaused  :: Bool
-  } deriving (Eq, Show)
+createTocker
+  :: MonadWidget t m
+  => TockerInputs t -> m (TockerOutputs t)
+createTocker inputs = do
+  rec elapsedD <-
+        fmap join .
+        widgetHold (pure (pure 0)) . leftmost $
+          [ pure (pure 0) <$ _tockerInputsReset inputs
+          , secondsFrom 0 <$ _tockerInputsStart inputs
+          , (\(v, ()) -> pure (constDyn v)) <$>
+            attach (current elapsedD) (_tockerInputsPause inputs)
+          , (\(v, ()) -> secondsFrom v) <$>
+            attach (current elapsedD) (_tockerInputsResume inputs)
+          ]
 
-timerBegin :: Functor f => (Seconds -> f Seconds) -> Timer -> f Timer
-timerBegin f t = (\x -> t { _timerBegin = x }) <$> f (_timerBegin t)
+  let alarmingE =
+        (attachWith
+           (\limit step -> step >= limit)
+           (_tockerInputsLimit inputs)
+           (updated elapsedD))
+  pure $ TockerOutputs elapsedD alarmingE
 
-timerPaused :: Functor f => (Bool -> f Bool) -> Timer -> f Timer
-timerPaused f t = (\x -> t { _timerPaused = x }) <$> f (_timerPaused t)
+secondsFrom
+  :: MonadWidget t m
+  => Seconds -> m (Dynamic t Seconds)
+secondsFrom initial = do
+  e <- startTickingE 1
+  (fmap ((+ initial) . Seconds) <$> count e)
 
-isRunning :: Timer -> Bool
-isRunning = isJust . _timerCurrent
+data MetronomeInputs t = MetronomeInputs {
+    _metronomeInputsRate  :: Dynamic t Rate
+  , _metronomeInputsStart :: Event t ()
+  , _metronomeInputsStop  :: Event t ()
+}
 
-stepTimer :: Timer -> Timer
-stepTimer t | _timerPaused t = t
-            | otherwise =
-  case _timerCurrent t of
-    Just stepping -> t { _timerCurrent = Just (succ stepping) }
-    Nothing       -> t { _timerCurrent = Just 0 }
+data MetronomeOutputs t = MetronomeOutputs {
+    _metronomeOutputsBeep  :: Event t ()
+  }
 
-resetTimer :: Timer -> Timer
-resetTimer t = t { _timerCurrent = Nothing }
+createMetronome :: MonadWidget t m => MetronomeInputs t -> m (MetronomeOutputs t)
+createMetronome inputs =
+  do startedOrNot <- accum (\x y -> y) False . leftmost $ [
+          True <$ _metronomeInputsStart inputs
+        , False <$ _metronomeInputsStop inputs
+      ]
 
--- type TimerEvent = Timer -> Timer
+     let timeD = rateToPeriod <$> (_metronomeInputsRate inputs)
+
+     fmap (MetronomeOutputs . fmapMaybe id . updated) .
+      fmap join .
+       widgetHold (pure (pure Nothing)) . fmap (\(rate, x) ->
+        if not x
+           then pure (pure Nothing)
+           else do e <- startTickingE rate
+                   -- We emit first because otherwise the first tick is delayed by `rate`
+                   holdDyn (Just ()) (Just () <$ e)) $ updated (zipDynWith (,) timeD startedOrNot)
+
 
 newtype Rate = Rate Integer deriving (Eq, Show, Ord, Enum)
 
-smallMinute = 5
-addMinute = timerBegin +~ smallMinute
-subtractMinute r = if _timerBegin r < smallMinute  then r else r & timerBegin -~ smallMinute
-
-addTime t = timerBegin +~ t
-subtractTime t = timerBegin -~ t
-
-data TimerState = Ready | Ticking | Paused | Alarming
-
-data TimerEvent = Reset | Tick | Pause
-
-applyState :: TimerEvent -> Timer -> Timer
-applyState Reset = resetTimer
-applyState Tick  = stepTimer
-applyState Pause = timerPaused %~ not
-
 main = do
    player <- liftIO $ createAudio "243748__unfa__metronome-2khz-strong-pulse.flac"
-   let initialTimer = Timer 10 Nothing False
-       initialMetronomeRate = Rate 80
+   let initialMetronomeRate = Rate 80
    mainWidget $ do
     el "div" $ text "Practice Pad"
     el "ul" $ do
-     (decrTE, incrTE) <- el "li" $ (,) <$> button "-" <*> button "+"
+      limits <- el "li" $ LimitInputs <$> (fmap (const 5) <$> button "+") <*> (fmap (const 5) <$> button "-")
+      tickerLimit <- createLimit limits 10
 
-     rec bb <- el "li" $ buttonDyn (dynText buttonText)
-         isStarted <- accum (&) False . mergeWith (.) $ [
-           not <$ bb
-           -- Decided that changing the timer by +/- a minute shouldn't affect the alarm
-           --  const False <$ decrTE,
-           --  const False <$ incrTE
-           ]
-         let buttonText = (\a -> if a then "Stop" else "Start") <$> isStarted
+      el "li" $ display (_limitOutsSeconds tickerLimit)
+      let limitB = current (_limitOutsSeconds tickerLimit)
+      tockerInputs <- el "li" $ do
+        startE <- button "start"
+        resetE <- button "reset"
+        pauseE <- button "pause"
+        resume <- button "resume"
+        pure $ TockerInputs limitB startE resetE pauseE resume
 
-         -- Timer should be in one of these states, with one of these action sets
-         -- Ready -> Start
-         -- Ticking -> Pause, Finish
-         -- Paused -> Resume, Finish
-         -- Alarming -> Finish
-
-     -- stream of timer events that starts and stops based on changes to isStarted
-     -- :: Event t TimerEvent
-     timerEvents <- countdownTimer (updated isStarted)
-
-     -- drive the current timer with the events out of timerEvents
-     -- :: Dynamic t Timer
-     activeTimerD <- accum (&) initialTimer . mergeWith (.) $ [
-          applyState <$> updated timerEvents
-        , addMinute <$ incrTE
-        , subtractMinute <$ decrTE
-        ]
-
-     el "li" $ display activeTimerD
+      tocker <- createTocker tockerInputs
+      el "li" $ display $ _tockerOutputsElapsed tocker
+      (el "li" . display) =<< holdDyn False (_tockerOutputsAlarming tocker)
+      pure ()
 
     el "ul" $ do
      ------------
      (decrB, incrB) <- el "li" $ (,) <$> button "-" <*> button "+"
-     metroBpmD <- accum (&) initialMetronomeRate . mergeWith (.) $ [pred <$ decrB, succ <$ incrB]
+     rateD <- accum (&) initialMetronomeRate . mergeWith (.) $ [pred <$ decrB, succ <$ incrB]
      bb2 <- el "li" $ button "Start/Stop Metronome"
-     let rateD = rateToPeriod <$> metroBpmD
-     el "li" $ display metroBpmD
+
+     el "li" $ display rateD
 
      startStopMetronomeE <- toggle False bb2
-     x <- metronomey . updated $ (,) <$> (rateToPeriod <$> metroBpmD) <*> startStopMetronomeE
-     let theJusts = fmapMaybe id $ updated x
-     addVoidAction ((Media.play player >> pure () ) <$ theJusts)
+
+     let starts = () <$ ffilter id (updated startStopMetronomeE)
+         stops = () <$ ffilter not (updated startStopMetronomeE)
+
+     MetronomeOutputs x <- createMetronome $ MetronomeInputs rateD starts stops
+     addVoidAction ((Media.play player >> pure () ) <$ x)
      pure ()
-
-countdownTimer :: MonadWidget t m => Event t Bool -> m (Dynamic t TimerEvent)
-countdownTimer =
-  fmap join .
-     widgetHold (pure (constDyn Reset)) . fmap (\x ->
-       if not x
-          then pure (constDyn Reset)
-          else do e <- startTickingE 1
-                  holdDyn Tick (const Tick <$> e)
-          )
-
-metronomey :: MonadWidget t m => Event t (NominalDiffTime, Bool) -> m (Dynamic t (Maybe ()))
-metronomey =
-  fmap join .
-     widgetHold (pure (pure Nothing)) . fmap (\(rate, x) ->
-       if not x
-          then pure (pure Nothing)
-          else do e <- startTickingE rate
-                  -- We emit first because otherwise the first tick is delayed by `rate`
-                  holdDyn (Just ()) (Just () <$ e))
 
 startTickingE :: MonadWidget t m => NominalDiffTime -> m (Event t Integer)
 startTickingE n = do
@@ -194,10 +177,6 @@ rateToPeriod (Rate r) = (60/) . fromRational . toRational $ r
 buttonDyn s = do
   (e, _) <- elAttr' "button" (Map.singleton "type" "button") s
   return $ domEvent Click e
-
-isJust :: Maybe a -> Bool
-isJust (Just _) = True
-isJust Nothing  = False
 
 -----------
 foreign import javascript unsafe "new Audio($1)"
